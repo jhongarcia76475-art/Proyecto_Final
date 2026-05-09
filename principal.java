a514a019c11536a3fafb6db8d43f990538c30482
miguetoro16 <miguetoro16@gmail.com>
2026-05-08 22:15:37 -0500

    se agregaron dos lineas

--- principal.java
+++ principal.java
@@ -1,5 +1,7 @@
 public class principal{
     public static void main(String[] args) {
         System.out.println("hola");
+        //aclaracion
+        System.out.println("hay que empesar a desarrollar el proyecto");
     }
 }
