e03270e9c90a524d0a968f796451cd3e4961dcab
Miguel angel Martínez parra <miguel.martinez53554@ucaldas.edu.co>
2026-06-22 19:16:33 -0500

    se creo el menu

--- principal.java
+++ principal.java
@@ -1,8 +1,21 @@
 public class principal{
     public static void main(String[] args) {
-        System.out.println("Sistema de gestion venta de zapatos");
-        System.out.println("-----------------------------------");
-        System.out.println("Que proceso desea realizar ");
+        System.out.println("          MENU DE OPCIONES              ");
+        System.err.println("_______________________________________");
+        System.out.println("|1. INGRESO DE EFECTIVO                |");
+        System.out.println("|______________________________________|");
+        System.out.println("|2. INGRESO DE REFERENCIAS             |");
+        System.err.println("|______________________________________|");
+        System.out.println("|3. INGRESO DE VENTAS                  |");
+        System.err.println("|______________________________________|");
+        System.out.println("|4. MOSTRAR INVENTARIO                 |");
+        System.err.println("|______________________________________|");
+        System.out.println("|5. MOSTRAR REPORTE DE VENTAS          |");
+        System.err.println("|______________________________________|");
+        System.out.println("|6. ACTUALIZAR INVENTARIO              |");
+        System.err.println("|______________________________________|");
+        System.out.println("|7. REPORTE GENERAL                    |");
+        System.err.println("|______________________________________|");
         
     }
 }
